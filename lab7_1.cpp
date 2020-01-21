b2d06ae38a16466261859632f48e6e621c57d85d
Apisit Ritreungroj <apisit_lon@hotmail.com>
2020-01-21 15:42:29 +0700

    completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,17 +1,22 @@
-#include <iostream>
-using namespace std;
+  #include <iostream>
+  using namespace std;
 
-int main()
-{
-  char rank;
-  cout << "Input your rank:";
-  cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
+  int main()
+  {
+    char rank;
+    cout << "Input your rank:";
+    cin >> rank;
+    
+    if (rank == 'S')
+      cout << "You have received Super Ultra Rare Unit!!!\n";
+    if (rank == 'A' || rank == 'S')
+      cout << "You have received 5 gems.\n";
+    if (rank == 'B' || rank == 'A' || rank == 'S')
+      cout << "You have received 1 gems.\n";
+    if (rank == 'C' || rank == 'B' || rank == 'A' || rank == 'S')
+      cout << "You have received 2000 coins.\n";
+    if (rank == 'D' || rank == 'C' || rank == 'B' || rank == 'A' || rank == 'S')
+      cout << "You have received very KAK items.\n";
+    
+    return 0;
   }
-  return 0;
-}
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,38 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+
+void RecieveAsset(string &status, long condition, string status1, string status2) {
+    long asset;
+    cout << "Input asset: ";
+    cin >> asset;
+    
+    status = asset > condition ? status1 : status2;
+}
+
+int main() {
+    short age;
+    string status = "";
+    cout << "Input age: ";
+    cin >> age;
+    
+    if (age <= 20) {
+        int height;
+        cout << "Input height: ";
+        cin >> height;
+        
+        if (height < 160) status = "UNFREIND";
+        else if (height < 175) status = "FRIEND";
+        else RecieveAsset(status, 69e+6, "MARRIED", "ONE-NIGHT-STAND");
+    }
+    else if (age < 30) {
+        RecieveAsset(status, 1e+7, "BEST FREIND", "UNFREIND");
+    }
+    else {
+        status = "UNFREIND";
+    }
+    
+    cout << status;
+    
+    return 0;
+}
